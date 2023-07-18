43ae712e263225537e7b7fece466e2aa37fa4799
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-07-18 18:19:36 +0200

    Create main.js

---
+++ 25_school_catalogue/main.js
@@ -0,0 +1,88 @@
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set numberOfStudents(nb_students) {
+    if (nb_students.isInteger()) this._numberOfStudents = nb_students;
+    else console.log("Invalid input: nb_students must be set to a Number");
+  }
+
+  quickFacts() {
+    console.log(
+      this.name +
+        " educates " +
+        this.numberOfStudents +
+        " students at the " +
+        this.level +
+        " school level."
+    );
+  }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    let index = Math.floor(Math.random() * (substituteTeachers.length - 1));
+    return substituteTeachers[index];
+  }
+}
+class Primary extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, "primary", numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+class Middle extends School {
+  constructor(name, numberOfStudents) {
+    super(name, "middle", numberOfStudents);
+  }
+}
+
+class High extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, "high", numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
+
+const lorraineHansbury = new Primary(
+  "Lorraine Hansbury",
+  514,
+  "Students must be picked up by a parent, guardian, or a family member over the age of 13."
+);
+
+lorraineHansbury.quickFacts();
+
+School.pickSubstituteTeacher([
+  "Jamal Crawford",
+  "Lou Williams",
+  "J. R. Smith",
+  "James Harden",
+  "Jason Terry",
+  "Manu Ginobli",
+]);
+
+const alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+console.log(alSmith.sportsTeams);
